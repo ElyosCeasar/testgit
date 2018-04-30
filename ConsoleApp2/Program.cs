3a50bc6e9c8c411843c399b9a580cb800dc6fb3c
Parseh <Parseh@DESKTOP-CSMSRVV>
2018-04-30 23:00:55 +0430

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x= {20 ,6 ,7 ,8 ,9,10 };
+            int y=5;
+            int i, j, temp;
+
+            for (i = y ; i > 1; i--)
+                for (j = 1; j < i; j++)
+                {
+                           if (x[j] > x[j + 1])
+                            {
+                    temp = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = temp;
+                            }
+                }
+            Console.WriteLine(x[0]);
+            Console.WriteLine(x[1]);
+            Console.WriteLine(x[2]);
+            Console.WriteLine(x[3]);
+            Console.WriteLine(x[4]);
+            Console.WriteLine(x[5]);
+            Console.ReadKey();
+        }
+    }
+}
